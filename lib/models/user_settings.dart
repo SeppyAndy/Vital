2b98ed1f5ad9d8f95a088266a49b729e2f74c7a8
SeppyAndy <seppy.rata@gmail.com>
2025-11-26 12:00:37 +1300

    Personalize nutrition macros from user settings

---
+++ lib/models/user_settings.dart
@@ -0,0 +1,15 @@
+class UserSettings {
+  int? age;
+  double? heightCm;
+  double? weightKg;
+  String? goal;
+  String? experienceLevel;
+
+  UserSettings({
+    this.age,
+    this.heightCm,
+    this.weightKg,
+    this.goal,
+    this.experienceLevel,
+  });
+}
